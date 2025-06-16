3a82445287c8ae1d660660e1e1b32800ecfd08d7
midou630 <abdelhamidstartup@gmail.com>
2025-06-16 02:38:01 +0100

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,52 +1,53 @@
 
-  $(function () {
+  (function ($) {
+  
+  "use strict";
 
     // MENU
-    $('.nav-link').on('click',function(){
+    $('.navbar-collapse a').on('click',function(){
       $(".navbar-collapse").collapse('hide');
     });
-
-
-    // AOS ANIMATION
-    AOS.init({
-      disable: 'mobile',
-      duration: 800,
-      anchorPlacement: 'center-bottom'
-    });
-
-
-    // SMOOTH SCROLL
-    $(function() {
-      $('.nav-link').on('click', function(event) {
-        var $anchor = $(this);
-          $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 0
-          }, 1000);
-            event.preventDefault();
-      });
-    });  
-
-
-    // PROJECT SLIDE
-    $('#project-slide').owlCarousel({
-      loop: true,
-      center: true,
-      autoplayHoverPause: false,
-      autoplay: true,
-      margin: 30,
-      responsiveClass:true,
-      responsive:{
-          0:{
-              items:1,
-          },
-          768:{
-              items:2,
-          }
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
       }
     });
 
-  });
+    $(window).on('scroll', function(){
+      function isScrollIntoView(elem, index) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(window).height()*.5;
+        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+          $(elem).addClass('active');
+        }
+        if(!(elemBottom <= docViewBottom)) {
+          $(elem).removeClass('active');
+        }
+        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
+        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      }
+      var timeline = $('#vertical-scrollable-timeline li');
+      Array.from(timeline).forEach(isScrollIntoView);
+    });
+  
+  })(window.jQuery);
 
-
-    
 
